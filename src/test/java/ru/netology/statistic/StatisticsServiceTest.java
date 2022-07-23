a8611844d3a5c31dfeab2d765fb2f810572701d1
taggertt <taggertt@mail.ru>
2022-07-23 14:42:54 +0500

    add 1 test case = 100% CC

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,5 +1,6 @@
 package ru.netology.statistic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,5 +17,17 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
